e8d6e5fa4354a2460f10a23decc5b96f6e5dc368
ShanmukhKarakavalasa <108892553+ShanmukhKarakavalasa@users.noreply.github.com>
2022-07-29 16:29:31 +0530

    Create add_two_numbers_as_linked_list.cpp

---
+++ Day5/add_two_numbers_as_linked_list.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode* ans=new ListNode(), *curr=ans;
+        int carry=0;
+        
+        while(l1||l2||carry)
+        {
+          if(l1){
+            carry+=l1->val;
+            l1=l1->next;
+         }
+          
+          if(l2)
+          {
+            carry+=l2->val;
+            l2=l2->next;
+          }
+          
+          curr->next=new ListNode(carry%10);
+          carry/=10;
+          curr=curr->next;
+        }
+        return ans->next;
+    }
+};
